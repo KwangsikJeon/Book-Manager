929fc717ea3d3d4d7467d676b9051a333dee040c
Raven <hong18s@gmail.com>
2012-08-02 20:19:30 +0900

    메뉴에 상응하는 함수들이 추가 되었으며 현재는 Insert 의 부분적인 기능이 구현되어 있다.

---
+++ functions.h
@@ -0,0 +1,7 @@
+#ifndef __FUNCTIONS_H__
+#define __FUNCTIONS_H__
+
+int menu_print(void);           /* 메뉴 구현 함수 원형 */
+void insert_book(void);          /* 메뉴 1번인 Insert, 도서추가 부분을 구현 */
+
+#endif // __FUNCTIONS_H__
